77f623c79b135fbc4b42e9972a53f4de1d784ae1
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-04-25 11:03:37 -0400

    Add calculate net worth script

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,11 @@
+// calculate area of rectangle//
+
+var num1 = 14
+var num2 = 2
+
+// Expression //
+
+area = num1 * num2
+
+// output //
+console.log("The area of the rectangle is  " + area );
---
+++ MathScripts/net_worth.js
@@ -0,0 +1,11 @@
+// Calculate net worth //
+
+var num1 = 500000;
+var num2 = 2000;
+
+// Expression//
+var answer = num1 -num2;
+
+// output //
+
+console.log("Your net worth is " + answer );
