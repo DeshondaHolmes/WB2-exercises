3bce9057c07a9c338d946fcdb66b6f7b3801aab6
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-04-25 12:16:07 -0400

    Add rule 72 calculation

--- MathScripts/rule_of_72.js
+++ MathScripts/rule_of_72.js
@@ -1,4 +1,17 @@
-// Calculate net worth //
 
-var assets
-var debts
+
+// sample inputs
+var savingsAccount = 10000;
+var interestRate = 0.06;
+var totalYears = 12;
+
+// calculations
+var totalTime =  totalYears / 0.06 ;
+var totalSavings = 10000 * 2
+
+
+// output
+
+console.log(
+    "At a " + interestRate.toFixed(2) +" interest rate"+ " your savings will be worth " + totalSavings + " in " + totalYears.toFixed(1) + " years.");
+   
--- tips.js
+++ tips.js
@@ -10,5 +10,6 @@ var totalDue = foodCost + tax + tip;
 // output
 console.log(
     "Food cost is " + foodCost + " and tax is " + tax);
-   console.log("Tip is " + tip.toFixed(2));
+   console.log("Tip is " + tip);
    console.log("Total Due is " + totalDue);
+   
