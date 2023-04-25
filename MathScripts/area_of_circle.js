dc099b7a90111cebf55486ba87c3712a6cdbfbfb
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-04-25 11:46:47 -0400

    Add calculate area of circle

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,11 @@
+// Calculate area of circle //
+var radius = 5;
+
+
+// Calculation //
+var areaOfCircle = Math.PI * (radius ** 2);
+
+
+
+// output
+console.log( "The area of a circle with radius" + radius + " is " + areaOfCircle);
