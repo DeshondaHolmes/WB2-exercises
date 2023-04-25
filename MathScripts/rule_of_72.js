311e110657df5600e82d9ded0b8c88cc86b92266
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-04-25 12:18:38 -0400

    Update variable to calculation

--- MathScripts/rule_of_72.js
+++ MathScripts/rule_of_72.js
@@ -4,10 +4,11 @@
 var savingsAccount = 10000;
 var interestRate = 0.06;
 var totalYears = 12;
+var doubleyear = 2;
 
 // calculations
-var totalTime =  totalYears / 0.06 ;
-var totalSavings = 10000 * 2
+var totalTime =  totalYears / interestRate ;
+var totalSavings = savingsAccount * doubleyear
 
 
 // output
