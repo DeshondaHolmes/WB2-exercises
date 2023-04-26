3fcaf20a2a8f975952a1a480883619f3c9faee67
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-04-26 10:09:39 -0400

    add inline script to html

--- MathScripts/taxes.js
+++ MathScripts/taxes.js
@@ -1,9 +1,14 @@
+"use strict"
 // calculate tax withholding
 var  mySalary= 500;
 var federalTax = 0.23;
 
 //formula
-var taxWithholding = (mySalary * federalTax)
+var taxWithholding = (mySalary * federalTax);
+var amountOfCheck = salary - withheldAmount;
 
 // output
 console.log("Federal tax withholding $ " + taxWithholding.toFixed(2) + " , is withheld every month" );
+
+console.log("You made $" = salary);
+console.log("But the government took $") 
---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,44 @@
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Jordan Q. Newprogrammer
+
+
+
+//Declare our inputt value
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+var result;
+
+//calculate the unknown
+
+
+var aInt= parseInt(a);
+var aInt= parseInt(b);
+var aInt= parseInt(c);
+var aInt= parseInt(d);
+
+var aFloat= parseFloat(a);
+var aFloat= parseFloat(b);
+var aFloat= parseFloat(c);
+var aFloat= parseFloat(d);
+
+
+result = parseInt(a);
+console.log()
+
+result = parseInt(b);
+console.log()
+
+result = parseInt(c);
+console.log()
+
+result = parseInt(d);
+console.log()
+
+
+//Display the value
+console.log(a)
+console.log(b)
+console.log(c)
